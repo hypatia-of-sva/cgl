5b77b6212811a8605385c92e9defeb7149c37c70
hypatia-of-sva <121448956+hypatia-of-sva@users.noreply.github.com>
2023-03-26 23:46:26 +0200

    added in nullptr macro

--- cgl.c
+++ cgl.c
@@ -11,59 +11,59 @@
 
 /* something about glClearDepthf, glDepthRangef.. */
 
+#ifndef __cplusplus
+#define nullptr NULL
+#endif
 
 
-PFNGLACTIVETEXTUREPROC glad_glActiveTexture = NULL;
-/*
-GLAPI PFNGLATTACHSHADERPROC glad_glAttachShader;
-GLAPI PFNGLBINDATTRIBLOCATIONPROC glad_glBindAttribLocation;
-GLAPI PFNGLBINDBUFFERPROC glad_glBindBuffer;
-GLAPI PFNGLBINDTEXTUREPROC glad_glBindTexture;
-GLAPI PFNGLBLENDCOLORPROC glad_glBlendColor;
-GLAPI PFNGLBUFFERDATAPROC glad_glBufferData;
-GLAPI PFNGLBUFFERSUBDATAPROC glad_glBufferSubData;
-GLAPI PFNGLBLENDEQUATIONPROC glad_glBlendEquation;
-GLAPI PFNGLBLENDEQUATIONSEPARATEPROC glad_glBlendEquationSeparate;
-GLAPI PFNGLBLENDFUNCPROC glad_glBlendFunc;
-GLAPI PFNGLBLENDFUNCSEPARATEPROC glad_glBlendFuncSeparate;
-GLAPI PFNGLCLEARPROC glad_glClear;
-GLAPI PFNGLCLEARCOLORPROC glad_glClearColor;
-GLAPI PFNGLCLEARDEPTHPROC glad_glClearDepth;
-GLAPI PFNGLCLEARSTENCILPROC glad_glClearStencil;
-GLAPI PFNGLCOLORMASKPROC glad_glColorMask;
-GLAPI PFNGLCOMPILESHADERPROC glad_glCompileShader;
-GLAPI PFNGLCOPYTEXIMAGE2DPROC glad_glCopyTexImage2D;
-GLAPI PFNGLCOPYTEXSUBIMAGE2DPROC glad_glCopyTexSubImage2D;
-GLAPI PFNGLCREATEPROGRAMPROC glad_glCreateProgram;
-GLAPI PFNGLCREATESHADERPROC glad_glCreateShader;
-GLAPI PFNGLCULLFACEPROC glad_glCullFace;
-GLAPI PFNGLDELETEBUFFERSPROC glad_glDeleteBuffers;
-GLAPI PFNGLDELETEPROGRAMPROC glad_glDeleteProgram;
-GLAPI PFNGLDELETESHADERPROC glad_glDeleteShader;
-GLAPI PFNGLDELETETEXTURESPROC glad_glDeleteTextures;
-GLAPI PFNGLDEPTHFUNCPROC glad_glDepthFunc;
-GLAPI PFNGLDEPTHMASKPROC glad_glDepthMask;
-GLAPI PFNGLDEPTHRANGEPROC glad_glDepthRange;
-GLAPI PFNGLDETACHSHADERPROC glad_glDetachShader;
-GLAPI PFNGLDISABLEPROC glad_glDisable;
-GLAPI PFNGLDISABLEVERTEXATTRIBARRAYPROC glad_glDisableVertexAttribArray;
-GLAPI PFNGLDRAWARRAYSPROC glad_glDrawArrays;
-GLAPI PFNGLDRAWELEMENTSPROC glad_glDrawElements;
-GLAPI PFNGLENABLEPROC glad_glEnable;
-GLAPI PFNGLENABLEVERTEXATTRIBARRAYPROC glad_glEnableVertexAttribArray;
-GLAPI PFNGLFINISHPROC glad_glFinish;
-GLAPI PFNGLFLUSHPROC glad_glFlush;
-GLAPI PFNGLFRONTFACEPROC glad_glFrontFace;
-GLAPI PFNGLGENBUFFERSPROC glad_glGenBuffers;
-GLAPI PFNGLGENTEXTURESPROC glad_glGenTextures;
-GLAPI PFNGLGETACTIVEATTRIBPROC glad_glGetActiveAttrib;
-GLAPI PFNGLGETBOOLEANVPROC glad_glGetBooleanv;
-GLAPI PFNGLGETFLOATVPROC glad_glGetFloatv;
-GLAPI PFNGLGETINTEGERVPROC glad_glGetIntegerv;
+PFNGLACTIVETEXTUREPROC glad_glActiveTexture = nullptr;
+PFNGLATTACHSHADERPROC glad_glAttachShader = nullptr;
+PFNGLBINDATTRIBLOCATIONPROC glad_glBindAttribLocation = nullptr;
+PFNGLBINDBUFFERPROC glad_glBindBuffer = nullptr;
+PFNGLBINDTEXTUREPROC glad_glBindTexture = nullptr;
+PFNGLBLENDCOLORPROC glad_glBlendColor = nullptr;
+PFNGLBUFFERDATAPROC glad_glBufferData = nullptr;
+PFNGLBUFFERSUBDATAPROC glad_glBufferSubData = nullptr;
+PFNGLBLENDEQUATIONPROC glad_glBlendEquation = nullptr;
+PFNGLBLENDEQUATIONSEPARATEPROC glad_glBlendEquationSeparate = nullptr;
+PFNGLBLENDFUNCPROC glad_glBlendFunc = nullptr;
+PFNGLBLENDFUNCSEPARATEPROC glad_glBlendFuncSeparate = nullptr;
+PFNGLCLEARPROC glad_glClear = nullptr;
+PFNGLCLEARCOLORPROC glad_glClearColor = nullptr;
+PFNGLCLEARDEPTHPROC glad_glClearDepth = nullptr;
+PFNGLCLEARSTENCILPROC glad_glClearStencil = nullptr;
+PFNGLCOLORMASKPROC glad_glColorMask = nullptr;
+PFNGLCOMPILESHADERPROC glad_glCompileShader = nullptr;
+PFNGLCOPYTEXIMAGE2DPROC glad_glCopyTexImage2D = nullptr;
+PFNGLCOPYTEXSUBIMAGE2DPROC glad_glCopyTexSubImage2D = nullptr;
+PFNGLCREATEPROGRAMPROC glad_glCreateProgram = nullptr;
+PFNGLCREATESHADERPROC glad_glCreateShader = nullptr;
+PFNGLCULLFACEPROC glad_glCullFace = nullptr;
+PFNGLDELETEBUFFERSPROC glad_glDeleteBuffers = nullptr;
+PFNGLDELETEPROGRAMPROC glad_glDeleteProgram = nullptr;
+PFNGLDELETESHADERPROC glad_glDeleteShader = nullptr;
+PFNGLDELETETEXTURESPROC glad_glDeleteTextures = nullptr;
+PFNGLDEPTHFUNCPROC glad_glDepthFunc = nullptr;
+PFNGLDEPTHMASKPROC glad_glDepthMask = nullptr;
+PFNGLDEPTHRANGEPROC glad_glDepthRange = nullptr;
+PFNGLDETACHSHADERPROC glad_glDetachShader = nullptr;
+PFNGLDISABLEPROC glad_glDisable = nullptr;
+PFNGLDISABLEVERTEXATTRIBARRAYPROC glad_glDisableVertexAttribArray = nullptr;
+PFNGLDRAWARRAYSPROC glad_glDrawArrays = nullptr;
+PFNGLDRAWELEMENTSPROC glad_glDrawElements = nullptr;
+PFNGLENABLEPROC glad_glEnable = nullptr;
+PFNGLENABLEVERTEXATTRIBARRAYPROC glad_glEnableVertexAttribArray = nullptr;
+PFNGLFINISHPROC glad_glFinish = nullptr;
+PFNGLFLUSHPROC glad_glFlush = nullptr;
+PFNGLFRONTFACEPROC glad_glFrontFace = nullptr;
+PFNGLGENBUFFERSPROC glad_glGenBuffers = nullptr;
+PFNGLGENTEXTURESPROC glad_glGenTextures = nullptr;
+PFNGLGETACTIVEATTRIBPROC glad_glGetActiveAttrib = nullptr;
+PFNGLGETBOOLEANVPROC glad_glGetBooleanv = nullptr;
+PFNGLGETFLOATVPROC glad_glGetFloatv = nullptr;
+PFNGLGETINTEGERVPROC glad_glGetIntegerv = nullptr;
 
 
-
-*/
 
 
 
